82d303f0526447f00fc14552621831d66c4b06e4
amdazm05 <amdali1998@yahoo.com>
2022-11-28 11:21:26 +0500

    feat: & untested: Added recieving functionality , yet to test, and updated gitignore

--- include/async_udp.hpp
+++ include/async_udp.hpp
@@ -1,17 +1,33 @@
 #ifndef  _ASYNC_UDP
 #define _ASYNC_UDP
 
+// Standard library imports
 #include <string>
+#include <iostream>
+// Boost imports
 #include <boost/asio.hpp>
+#include <boost/bind.hpp>
 
 class Async_UDP_server
 {
     public:
-        Async_UDP_server();
+        Async_UDP_server()=delete; // This constructor is not usuable
+        // IO service 
+        Async_UDP_server(boost::asio::io_service &ioservice, int PortNum);
     private:
-        // TODO 
-
+        //  Member functions to expose
+        void start_receiver();
+        void handle_receieve(
+                const boost::system::error_code & error_caught,
+                size_t recievedByteCount 
+        );
+        boost::asio::ip::udp::socket _socket;
+        boost::asio::ip::udp::endpoint _endpoint;
+        std::array<char,1> recieve_buffer;
 };
 
+// References used :
+// https://www.boost.org/doc/libs/1_37_0/doc/html/boost_asio/tutorial/tutdaytime6/src.html
+// https://www.boost.org/doc/libs/1_66_0/doc/html/boost_asio/reference/ip__udp/socket.html
 
 #endif //_ASYNC_UDP
--- src/async_udp.cc
+++ src/async_udp.cc
@@ -1,1 +1,44 @@
 #include <async_udp.hpp>
+
+Async_UDP_server::Async_UDP_server(boost::asio::io_service &ioservice, int PortNum)
+    : 
+    // Bind socket to the portnumber passed
+    _socket(  ioservice, 
+              boost::asio::ip::udp::endpoint(
+                boost::asio::ip::udp::v4(),
+                PortNum
+                )
+           ),
+    // Make sure recieve buffer is empty on initialisation
+    recieve_buffer({})
+{
+    std::cout<<"******** --- UDP SERVER INITIALISED --- ********"<<std::endl;
+}
+
+/// @brief start recieving bytes from any client and call the token below
+/// @param none
+void Async_UDP_server::start_receiver()
+{
+    _socket.async_receive_from(
+        recieve_buffer,_endpoint,
+        boost::bind
+            (
+                &Async_UDP_server::handle_receieve,
+                this,
+                boost::asio::placeholders::error,
+                boost::asio::placeholders::bytes_transferred
+            )
+    );
+}
+
+/// @brief kind of like a call back
+/// @param none
+
+void Async_UDP_server::handle_receieve(
+        const boost::system::error_code & error_caught,
+        size_t recievedByteCount 
+    )
+{
+    std::cout<<"Handling recieved bytes"<<std::endl;
+    std::cout<<"Recieved character :" << recieve_buffer.at(1)<<std::endl;
+}
